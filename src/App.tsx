23f8dc6a47cf1b0142ff48ee5c8c89e3401ba67c
MonKokoro <735335659@qq.com>
2023-10-23 11:23:58 +0800

    引入windi，计划搭建更完善的全局换肤配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import React, { createRef } from 'react';
-import { BrowserRouter, Routes, Route, RouterProvider } from 'react-router-dom';
-
-// import Login from './page/login';
-// import Layout from './layout';
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
 
 import router from './router'
 
 function App() {
     return <RouterProvider router={router} />
-    // return <BrowserRouter>
-    //     <Routes>
-    //         <Route path="/login" element={<Login />}></Route>
-    //         <Route path="*" element={<Layout />}></Route>
-    //     </Routes>
-    // </BrowserRouter>
 }
 
 export default App;
